c0ce4c9e842c22bae9a7418c7db629586eb3c8ca
mrjvs <jellevs@gmail.com>
2023-10-16 22:25:37 +0200

    Fix forgotten awaits + prevent double reading

--- src/utils/sending.ts
+++ src/utils/sending.ts
@@ -1,11 +1,10 @@
 import { H3Event, EventHandlerRequest } from 'h3';
 
-export function sendJson(ops: {
+export async function sendJson(ops: {
   event: H3Event<EventHandlerRequest>;
   data: Record<string, any>;
   status?: number;
 }) {
   setResponseStatus(ops.event, ops.status ?? 200);
-  appendResponseHeader(ops.event, 'content-type', 'application/json');
-  send(ops.event, JSON.stringify(ops.data, null, 2));
+  await send(ops.event, JSON.stringify(ops.data, null, 2), 'application/json');
 }
